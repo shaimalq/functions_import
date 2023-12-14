d576b9e184af397cb15f051298ba10cef3f18831
shaimalq <146936338+shaimalq@users.noreply.github.com>
2023-12-14 22:11:22 +0100

    Add files via upload

---
+++ fonctions.py
@@ -0,0 +1,17 @@
+def signe(a,b):
+    if a*b>0:
+        print("a et b ont meme signe")
+    else:
+        print("a et b ont signe different")
+
+def maximum(a,b):
+    if a>b:
+        print("le maximum est:", a)
+    else:
+        print("le maximum est:", b)
+        
+def minimum(a,b):
+    if a<b:
+        print("le minimum est:", a)
+    else:
+        print("le minimum est:", b)
